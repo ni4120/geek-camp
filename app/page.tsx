5cbefcbe0f8554414baafa8859998cc91aa2faf2
KaimuSeino <skim07a19yki@icloud.com>
2024-10-31 22:41:59 +0900

    #1 HostFormを`/`ルートに追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import HostForm from "@/components/host-form"
+
 export default function Home() {
-  return (
-    <div className="flex h-full justify-center items-center">
-      Hello AI大喜利!
-    </div>
-  );
+    return (
+        <main className="flex h-full justify-center items-center">
+            <div className="flex flex-col items-center justify-center space-y-14">
+                <h2 className="text-3xl font-bold text-center">
+                    大喜利部屋を作成しましょう
+                </h2>
+                <HostForm />
+            </div>
+        </main>
+    )
 }
