522f1d400b59bfc27ec30fd79113a78c043b90c6
KaimuSeino <skim07a19yki@icloud.com>
2024-11-02 18:25:17 +0900

    #34 roomを取得する関数を追加

---
+++ data/rooms.ts
@@ -0,0 +1,14 @@
+import { db } from "@/lib/db"
+
+export const getRoomByRoomId = (roomId: string) => {
+    try {
+        const room = db.rooms.findFirst({
+            where: {
+                id: roomId,
+            }
+        })
+        return room      
+    } catch {
+        return null
+    }
+}
