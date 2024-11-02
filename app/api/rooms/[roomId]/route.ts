4aa6c6128e25d68083dc6ec112d28c73f509284d
kokoro <lily.koko412@gmail.com>
2024-11-02 21:41:10 +0900

    format修正

--- app/api/rooms/[roomId]/route.ts
+++ app/api/rooms/[roomId]/route.ts
@@ -2,23 +2,26 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
 export async function PATCH(
-    request: Request,
-    { params }: { params: { roomId: string } },
+  request: Request,
+  { params }: { params: { roomId: string } },
 ) {
-    const body = await request.json();
-    const roomId = (params.roomId); 
-    try {
-        const updatedRoom = await db.rooms.update({
-            where: {
-                id: roomId,
-            },
-            data: {
-                status: body.status,
-            },
-        });
-        return NextResponse.json(updatedRoom);
-    } catch {
-        console.error(Error);
-        return NextResponse.json({ error: "Error updating room status" }, { status: 500 });
-    }
+  const body = await request.json();
+  const roomId = params.roomId;
+  try {
+    const updatedRoom = await db.rooms.update({
+      where: {
+        id: roomId,
+      },
+      data: {
+        status: body.status,
+      },
+    });
+    return NextResponse.json(updatedRoom);
+  } catch {
+    console.error(Error);
+    return NextResponse.json(
+      { error: "Error updating room status" },
+      { status: 500 },
+    );
+  }
 }
