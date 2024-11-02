e66afb296269c5b08a7d5a102c0980d1da6a8c01
suba <84484832+suba327777@users.noreply.github.com>
2024-11-02 15:29:17 +0900

    Questionsテーブルのidをrandom_uuidにする

--- prisma/migrations/20241102062806_add_questions_table/migration.sql
+++ prisma/migrations/20241102062806_add_questions_table/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "Questions" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "content" TEXT NOT NULL,
 
     CONSTRAINT "Questions_pkey" PRIMARY KEY ("id")
