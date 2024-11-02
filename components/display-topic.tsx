816446d48129ddf7b2b1d22eb428f42e61bb99a7
KaimuSeino <skim07a19yki@icloud.com>
2024-11-03 00:52:21 +0900

    #50 formatの修正

--- components/display-topic.tsx
+++ components/display-topic.tsx
@@ -1,26 +1,27 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
-
 
 interface DisplayTopicProps {
   question: {
     id: string;
     content: string;
     createdAt: Date;
-  } | null
+  } | null;
 }
 
 const DisplayTopic = ({ question }: DisplayTopicProps) => {
-  const router = useRouter()
+  const router = useRouter();
   if (!question) {
-    router.push("/")
+    router.push("/");
   } else {
     return (
       <div className="mr-4 text-3xl font-bold">
         お題
         <span className="ml-4 text-2xl">
-          {question.content ? question.content : "お題を取得できませんでした..."}
+          {question.content
+            ? question.content
+            : "お題を取得できませんでした..."}
         </span>
       </div>
     );
--- data/questions.ts
+++ data/questions.ts
@@ -1,14 +1,14 @@
-import { db } from "@/lib/db"
+import { db } from "@/lib/db";
 
 export const getLatestQuestion = async () => {
-    try {
-        const question = await db.questions.findFirst({
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
-        return question;
-    } catch {
-        return null;
-    }
-}
+  try {
+    const question = await db.questions.findFirst({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return question;
+  } catch {
+    return null;
+  }
+};
