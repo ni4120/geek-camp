396a3046f9302d6fb1403ee83e87e520e918ac82
kokoro <lily.koko412@gmail.com>
2024-11-02 14:26:08 +0900

    format

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -3,20 +3,20 @@ import { db } from "@/lib/db";
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(req: Request) {
-    const { name, hostId } = await req.json();
-    const sharedUrl = `${process.env.BASE_URL || "http://localhost:3000"}/room/${uuidv4()}`;
+  const { name, hostId } = await req.json();
+  const sharedUrl = `${process.env.BASE_URL || "http://localhost:3000"}/room/${uuidv4()}`;
 
-    try {
-        const room = await db.rooms.create({
-            data: {
-                name,
-                hostId,
-                sharedUrl,
-                status: 'WAITING',
-            },
-        });
-        return NextResponse.json(room);
-    } catch (error) {
-        return NextResponse.json({ error: 'Error creating room' }, { status: 500 });
-    }
+  try {
+    const room = await db.rooms.create({
+      data: {
+        name,
+        hostId,
+        sharedUrl,
+        status: "WAITING",
+      },
+    });
+    return NextResponse.json(room);
+  } catch (error) {
+    return NextResponse.json({ error: "Error creating room" }, { status: 500 });
+  }
 }
