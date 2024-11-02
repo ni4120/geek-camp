3c4bafe808ccb2929747a24584a97af98fe7547d
KaimuSeino <skim07a19yki@icloud.com>
2024-11-03 02:05:28 +0900

    #54 formatの修正

--- app/api/answers/route.ts
+++ app/api/answers/route.ts
@@ -2,25 +2,25 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const { roomUsers, roomId, questionId } = await req.json();
+  const { roomUsers, roomId, questionId } = await req.json();
 
-    if (!roomUsers || !roomId || !questionId) {
-        return new Response("Missing parameters", { status: 400 });
-    }
+  if (!roomUsers || !roomId || !questionId) {
+    return new Response("Missing parameters", { status: 400 });
+  }
 
-    try {
-        await db.answers.createMany({
-            data: roomUsers.map((user: any) => ({
-                userId: user.userId,
-                roomId: roomId,
-                questionId: questionId,
-                content: "",
-                isCompleted: false,
-            })),
-        })
+  try {
+    await db.answers.createMany({
+      data: roomUsers.map((user: any) => ({
+        userId: user.userId,
+        roomId: roomId,
+        questionId: questionId,
+        content: "",
+        isCompleted: false,
+      })),
+    });
 
-        return new NextResponse(null, { status: 201 });
-    } catch {
-        return NextResponse.json({ error: "error not create answers" })
-    }
+    return new NextResponse(null, { status: 201 });
+  } catch {
+    return NextResponse.json({ error: "error not create answers" });
+  }
 }
