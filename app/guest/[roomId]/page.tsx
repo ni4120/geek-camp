ec7be3cb62e079dc6a6bb88e575725bc0620cf9a
KaimuSeino <skim07a19yki@icloud.com>
2024-11-02 14:06:28 +0900

    #8 formatの修正

--- app/guest/[roomId]/page.tsx
+++ app/guest/[roomId]/page.tsx
@@ -1,20 +1,16 @@
 import UserForm from "@/components/user-form";
 
-const GuestRoomIdPage = ({
-    params
-}: {
-    params: { roomId: string }
-}) => {
-    return (
-        <main className="flex h-full justify-center items-center">
-            <div className="flex flex-col items-center justify-center space-y-14">
-                <h2 className="text-3xl font-bold text-center">
-                    <span>{"[部屋の名前]"}</span>部屋に招待されました
-                </h2>
-                <UserForm />
-            </div>
-        </main>
-    );
-}
- 
+const GuestRoomIdPage = ({ params }: { params: { roomId: string } }) => {
+  return (
+    <main className="flex h-full justify-center items-center">
+      <div className="flex flex-col items-center justify-center space-y-14">
+        <h2 className="text-3xl font-bold text-center">
+          <span>{"[部屋の名前]"}</span>部屋に招待されました
+        </h2>
+        <UserForm />
+      </div>
+    </main>
+  );
+};
+
 export default GuestRoomIdPage;
