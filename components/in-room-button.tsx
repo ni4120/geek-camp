a7ade722a807f0eb4185610400362b8f8e6b53ee
KaimuSeino <skim07a19yki@icloud.com>
2024-11-02 11:14:11 +0900

    #3 InRoomButtonコンポーネントを作成

---
+++ components/in-room-button.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+
+const InRoomButton = () => {
+    const router = useRouter()
+    const handleInRoom = () => {
+        router.push("http://localhost:3000/entrance/{room_id}?u_id={user_id}")
+    }
+    return (
+        <Button
+            variant="outline"
+            onClick={handleInRoom}
+            className="px-10"
+        >
+            入室
+        </Button>
+    );
+}
+ 
+export default InRoomButton;
