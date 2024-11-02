59e360d491ed51d5b0b519d606ca8f65a5156642
KaimuSeino <skim07a19yki@icloud.com>
2024-11-02 18:26:04 +0900

    #34 ShareUrlPageでroomのsahredUrlを取得しコピーできるようにセット。

--- app/create/[userId]/[roomId]/page.tsx
+++ app/create/[userId]/[roomId]/page.tsx
@@ -1,12 +1,22 @@
 import InRoomButton from "@/components/in-room-button";
 import ShareUrlForm from "@/components/share-url-form";
+import { getRoomByRoomId } from "@/data/rooms";
 
-const ShareUrlPage = () => {
-  const url = "https://share-url";
+interface ShareUrlPageProps {
+  params: {
+    roomId: string;
+  }
+}
+
+const ShareUrlPage = async ({
+  params
+}: ShareUrlPageProps) => {
+  const { roomId } = await params
+  const room = await getRoomByRoomId(roomId)
   return (
     <main className="flex flex-col h-full justify-center items-center gap-y-10">
       <h2 className="text-3xl font-bold">部屋のURLを共有しましょう</h2>
-      <ShareUrlForm url={url} />
+      <ShareUrlForm url={room?.sharedUrl!} />
       <InRoomButton />
     </main>
   );
