8f38dd6110e76bf4c7ef95213feeb347c774e449
suba <84484832+suba327777@users.noreply.github.com>
2024-11-03 12:47:16 +0900

    指定したroomIdの情報を取得するapi

--- app/api/rooms/[roomId]/route.ts
+++ app/api/rooms/[roomId]/route.ts
@@ -1,6 +1,23 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
+export async function GET(
+  { params }: { params: { roomId: string } },
+) {
+  console.log(params.roomId)
+  const roomId = params.roomId;
+  try {
+    const room = db.rooms.findFirst({
+      where: {
+        id: roomId,
+      },
+    });
+    return NextResponse.json(room, { status: 200 });
+  } catch (error) {
+    console.log("Error fetching room:", error);
+    return NextResponse.json({ error: "Error fetching room" }, { status: 500 });
+  }
+}
 export async function PATCH(
   request: Request,
   { params }: { params: { roomId: string } },
