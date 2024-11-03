a4fcbf33573249c8330cf1209ad65772e53d3e01
suba <84484832+suba327777@users.noreply.github.com>
2024-11-03 12:47:30 +0900

    entrance componentに userIdを渡す

--- app/entrance/[roomId]/[userId]/page.tsx
+++ app/entrance/[roomId]/[userId]/page.tsx
@@ -12,12 +12,10 @@ const EntranceRoomIdAndUserIdPage = async ({
 }: EntranceRoomIdPageProps) => {
   /** paramsから`roomId`と`userId`を取得 */
   const { roomId, userId } = await params;
-  console.log(`roomId: ${roomId}`);
-  console.log(`userId: ${userId}`);
   /** TODO: 部屋に参加しているusers情報をリアルタイムで取得 */
   return (
     <main className="flex h-full justify-center items-center">
-      <Entrance roomId={roomId} />
+      <Entrance roomId={roomId} userId={userId} />
     </main>
   );
 };
